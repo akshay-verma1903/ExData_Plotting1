2f3647f356a3deb808ac3adc453f769a4910e26c
akshay-verma1903 <52961641+akshay-verma1903@users.noreply.github.com>
2020-06-20 18:55:32 +0530

    Add files via upload

---
+++ Plot2.R
@@ -0,0 +1,6 @@
+data<-read.table("household_power_consumption.txt",sep=";",header=TRUE,stringsAsFactors=FALSE, dec=".")
+power_consumption<-subset(data,data$Date %in% c("1/2/2007","2/2/2007"))
+date_time <- strptime(paste(power_consumption$Date,power_consumption$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+png("plot2.png",width=480,height=480)
+plot(date_time,as.numeric(power_consumption$Global_active_power),col="red",type="l" ,ylab="Global Active Power (kilowatts)")
+dev.off()
---
+++ Plot4.R
@@ -0,0 +1,19 @@
+data<-read.table("household_power_consumption.txt",sep=";",header=TRUE,stringsAsFactors=FALSE, dec=".")
+power_consumption<-subset(data,data$Date %in% c("1/2/2007","2/2/2007"))
+date_time <- strptime(paste(power_consumption$Date,power_consumption$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+
+plot(date_time, as.numeric(power_consumption$Global_active_power), type="l", xlab="", ylab="Global Active Power", cex=0.2)
+
+plot(date_time, as.numeric(power_consumption$Voltage), type="l", xlab="datetime", ylab="Voltage")
+
+plot(date_time, as.numeric(power_consumption$Sub_metering_1), type="l", ylab="Energy Submetering",xlab="")
+lines(date_time, as.numeric(power_consumption$Sub_metering_2), type="l", col="red")
+lines(date_time, as.numeric(power_consumption$Sub_metering_3), type="l", col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+
+plot(date_time, as.numeric(power_consumption$Global_reactive_power), type="l", xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
